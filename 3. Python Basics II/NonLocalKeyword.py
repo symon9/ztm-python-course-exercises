982837786ecd2463d4549638ea56265b0050c4ea
Andrei Neagoie <10776230+aneagoie@users.noreply.github.com>
2026-03-19 13:33:26 -0400

    Update NonLocalKeyword.py

--- 3. Python Basics II/NonLocalKeyword.py
+++ 3. Python Basics II/NonLocalKeyword.py
@@ -1,16 +1,15 @@
-# Nonlocal Keyword
-
-
+# Scope - what variables do I have access to?
 def outer():
-    x = 'local'
-
+    x = "local"
     def inner():
         nonlocal x
-        x = 'nonlocal'
-        print('inner:', x)
-
+        x = "nonlocal"
+        print("inner:", x)
     inner()
-    print('outer:', x)
-
-
+    print("outer:", x)
 outer()
+
+#1 - start with local
+#2 - Parent local?
+#3 - global
+#4 - built in python functions
